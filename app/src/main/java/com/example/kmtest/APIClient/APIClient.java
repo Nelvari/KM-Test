3b6668ea20a46583d94eb6f9ea18f0c683dadfc3
Nelvari <nelvari2018rplrus2@gmail.com>
2023-12-22 20:11:09 +0700

    delete DataModel

--- app/src/main/java/com/example/kmtest/APIClient/APIClient.java
+++ app/src/main/java/com/example/kmtest/APIClient/APIClient.java
@@ -1,8 +1,5 @@
 package com.example.kmtest.APIClient;
 
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 public class APIClient {
 
     public static final String BASE_URL = "https://reqres.in/api/users?page=1&per_page=12/";
